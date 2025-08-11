a86dd8225ec8636599b94834fe18cdcf4a6d04e6
Shannon <shannond050@outlook.com>
2025-08-11 21:35:00 +0200

    Reading list update

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,40 @@ const books = [
   },
 ];
 
+const list = document.getElementById("reading-list");
+
+books.forEach((book) => {
+  const li = document.createElement("li");
+
+  li.style.backgroundColor = book.alreadyRead ? "green" : "red"; // green or red
+
+  li.style.display = "flex";
+  li.style.alignItems = "center";
+  li.style.gap = "1em";
+  li.style.padding = "1em";
+  li.style.marginBottom = "1em";
+  li.style.borderRadius = "8px";
+
+  const img = document.createElement("img");
+  img.src = book.bookCoverImage;
+
+  const info = document.createElement("div");
+
+  const title = document.createElement("p");
+  title.textContent = book.title;
+  title.style.fontWeight = "bold";
+  title.style.fontSize = "1.1em";
+  title.style.margin = "0";
+
+  const author = document.createElement("p");
+  author.textContent = `by ${book.author}`;
+  author.style.margin = "0";
+  author.style.fontStyle = "italic";
+
+  info.appendChild(title);
+  info.appendChild(author);
+
+  li.appendChild(img);
+  li.appendChild(info);
+  list.appendChild(li);
+});
